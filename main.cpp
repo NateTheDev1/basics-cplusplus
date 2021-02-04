69e8e0eb25c9d8d780b03623cf9bfa418767029a
NateTheDev1 <nthnlrichards@gmail.com>
2021-02-04 17:16:09 -0500

    Working on variables.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 using namespace std;
+
+
 int main() {
-    cout << "Hello, World!" << endl;
+    // console out - end line
+    string characterName = "Nate";
+    int characterAge = 21;
+
+    cout << "There once was a man named " + characterName << endl;
+    cout << "He was " << characterAge << " years old" << endl;
+    cout << "He liked the name " << characterName << endl;
+    cout << "But he did not like being " << characterAge << endl;
+
     return 0;
 }
