3929af96d977291c22cf1a51683e83ada8f6b9c7
NateTheDev1 <nthnlrichards@gmail.com>
2021-02-04 16:15:36 -0500

    Initial

---
+++ cmake-build-debug/CMakeFiles/Basics.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "Basics.exe"
+  "Basics.exe.manifest"
+  "Basics.pdb"
+  "CMakeFiles/Basics.dir/main.cpp.obj"
+  "libBasics.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Basics.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
