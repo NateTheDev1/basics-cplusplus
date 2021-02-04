15349831c6bd226204d3dc8e77359ae78b3b4d30
NateTheDev1 <nthnlrichards@gmail.com>
2021-02-04 17:16:09 -0500

    Done with variables.

--- main.cpp
+++ main.cpp
@@ -1,16 +1,28 @@
 #include <iostream>
+
 using namespace std;
 
 
 int main() {
-    // console out - end line
-    string characterName = "Nate";
-    int characterAge = 21;
+//    console out - end line
+//    string characterName = "Nate";
+//    int characterAge = 21;
+//
+//    cout << "There once was a man named " << characterName << endl;
+//    cout << "He was " << characterAge << " years old" << endl;
+//    cout << "He liked the name " << characterName << endl;
+//    cout << "But he did not like being " << characterAge << endl;
 
-    cout << "There once was a man named " + characterName << endl;
-    cout << "He was " << characterAge << " years old" << endl;
-    cout << "He liked the name " << characterName << endl;
-    cout << "But he did not like being " << characterAge << endl;
+//    char grade = 'A';
+//    string phrase = "Giraffe Academy";
+//    int age = 50;
+//    int negativeAge = -50;
+//    float younging = 5.5;
+//    double gpa = 2.3;
+
+
+
+
 
     return 0;
 }
