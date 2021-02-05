ced103d2b818fb80ee9dadbf4e37cfd45aff5569
NateTheDev1 <nthnlrichards@gmail.com>
2021-02-05 10:16:11 -0500

    Done with basic math operations.main

--- main.cpp
+++ main.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
-
-
 
 
 int main() {
@@ -37,6 +36,28 @@ int main() {
 //    cout << phrase.find('A', 0) << endl;
 //
 //    cout << phrase.substr(1, 5) << endl;
+
+//      cout << 10 % 3 << endl;
+//      cout << (4 + 5) * 10 << endl;
+//
+//      int wnum = 5;
+//      double dnum = 5.5;
+//
+//      wnum++;
+//
+//      cout << wnum << endl;
+//
+//      cout << 5.5 + 9 << endl;
+//
+//      cout << 10 / 3 << endl;
+//
+//      cout << pow(2, 5) << endl;
+//      cout << sqrt(35) << endl;
+//      cout << ceil(4.1) << endl;
+//      cout << floor(4.6) << endl;
+//      cout << fmax(3, 10) << endl;
+//      cout << fmin(3, 10) << endl;
+
 
 
     return 0;
